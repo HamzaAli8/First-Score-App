9bf18e18675d3a2d4d12cb84fb17b40d2ba3237a
Hamza Ali <hamzamalimaad@gmail.com>
2022-04-08 19:54:40 +0300

    update

--- src/main/java/com/livescore/app/model/PlayerDetailResponse.java
+++ src/main/java/com/livescore/app/model/PlayerDetailResponse.java
@@ -8,4 +8,15 @@ import lombok.*;
 @NoArgsConstructor
 @ToString
 public class PlayerDetailResponse {
+
+    private Integer id;
+    private String name;
+    private String [] nationalities;
+    private String fullName;
+    private String dob;
+    private String height;
+    private String weight;
+    private String foot;
+    private String photoURL;
+
 }
---
+++ src/main/java/com/livescore/app/service/PlayerDetailService.java
@@ -0,0 +1,4 @@
+package com.livescore.app.service;
+
+public class PlayerDetailService {
+}
