16683dddc7d6973fadc7c5762dc1212170834fd0
Hamza Ali <hamzamalimaad@gmail.com>
2022-05-31 14:47:17 +0300

    update

---
+++ src/main/resources/static/js/news.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+  // Build the news grid
+  var container = document.querySelector('#container');
+  var msnry = new Masonry( container, {
+    // options
+    columnWidth: 326,
+    itemSelector: '.item'
+  });
+
+ });
